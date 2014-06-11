f137514fffa410de4a179e94ee64ca4cc724d2a1
EendiVidi <chrisgoodman14@gmail.com>
2014-06-11 13:51:54 -0700

    fixed mongoid error

---
+++ config/heroku_env.rb
@@ -0,0 +1,8 @@
+# This file contains the ENV vars necessary to run the app locally.  
+# Some of these values are sensitive, and some are developer specific.
+#
+# DO NOT CHECK THIS FILE INTO VERSION CONTROL
+
+ENV["beeminder_client_id"] = '9nb98hwxlaxfwyrewg5drjzgpmccgnv'
+ENV["beeminder_client_secret"] = 'lbf2pz7cz0kk5why9ij62d3awygt0hg'
+#ENV["SECRET_TOKEN"] = '0000aaaammmmaaaaaoiojuiojghjklikikijujua3edfcvfrewwsdfre32123edfccccccxzai988ui89uih878h7h87'
